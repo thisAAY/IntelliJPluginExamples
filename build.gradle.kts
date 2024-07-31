c252871edac90e0c75b7e18c929d16a2d5c97db4
Ahmed Ali <ahmedaliaay@gmail.com>
2024-07-31 22:57:13 +0300

    Remove version catalg

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     id("java")
-    id("version-catalog")
     kotlin("plugin.serialization") version "2.0.0"
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.intellij)
+    id("org.jetbrains.kotlin.jvm") version "2.0.0"
+    id("org.jetbrains.intellij.platform") version "2.0.0"
 }
 
 group = "today.thisaay"
@@ -41,7 +40,10 @@ dependencies {
     intellijPlatform {
         instrumentationTools()
         androidStudio("2024.1.1.12")
-        bundledPlugins("org.jetbrains.kotlin","com.intellij.java")
+        bundledPlugins(
+            "org.jetbrains.kotlin",
+            "com.intellij.java",
+        )
     }
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
